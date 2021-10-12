8e608a71fbb6d78b510b7306e73e492a319f6191
Simon <simon_jacobs_1990@hotmail.co.uk>
2021-10-12 16:22:00 +0000

    Added to list of students

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# printing the list of students
 puts "The students of Villains Academy"
 puts "-------------"
 puts "Dr. Hannibal Lecter"
@@ -11,3 +12,8 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+print "Overall, we have "
+# printing the total number of students
+print 11
+# it's important that print() doesn't add new line characters
+puts " great students"
