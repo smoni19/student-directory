bb75030343d7fb6ef7710a766837a0a30058a69a
Simon <simon_jacobs_1990@hotmail.co.uk>
2021-10-12 17:15:34 +0000

    User can now input names into the student array

--- directory.rb
+++ directory.rb
@@ -1,20 +1,5 @@
-# array of students
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
-
 def print_header
-  puts "-----The students of Villains Academy-----"
+  puts "-----The students of my Makers cohort-----"
   puts "------------------------------------------"
 end
 
@@ -28,6 +13,22 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  students = []
+  name = gets.chomp
+  #!name.empty? = while the name is not empty
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  #returning the array. ruby doesn't need "return"
+  students
+end
+
+students = input_students
 print_header
 print(students)
 print_footer(students)
